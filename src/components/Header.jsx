cd9ad7c943fd27b30ebe9bb9c4e063ebca408e21
Ali Sheikh <l_lawliet786@hotmail.com>
2025-05-27 21:46:02 -0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+
+    return(
+        <>
+            <header>
+                <nav>
+                    <img className="globe" src="/src/imgs/globe.jpg" alt="globe"></img>
+                    <h1 className="title">places I want to travel.</h1>
+                </nav>
+            </header>
+        
+        
+        
+        </>
+    )
+}
