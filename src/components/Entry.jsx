1be97c0efddda4b030e1c82bbaa30d4bdb10f42e
Ali Sheikh <l_lawliet786@hotmail.com>
2025-05-30 22:05:06 -0400

    small tweaks

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,12 +2,14 @@ export default function Entry() {
 
     return (
         <>
-            <article>
-                <img src="/src/imgs/marker.png" alt="marker"></img>
-                <p>Location:
-                    Japan
-                    
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">Google Maps Link:</a>
+            <article className="entry">
+                <img className ="marker" src="/src/imgs/marker.png" alt="marker"></img>
+                <h1>Japan</h1>
+                <p>    
+                    <a href="https://www.google.com/maps/place/Mount+Fuji/
+                    @35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s
+                    0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3
+                    606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">Google Maps Link:</a>
 
                     Dates:
                     12 Jan, 2021 - 24 Jan, 2021
